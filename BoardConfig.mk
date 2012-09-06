7bb6795042bed7c5df1df7b61dead7cd5dc9b423
kfazz <kfazz01@gmail.com>
2012-09-06 00:33:47 -0400

    attempt to build wifi module
    Change-Id: I32e1a63dedbdf4f2996c274fdb841c162365714f

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -173,33 +173,13 @@ TARGET_KERNEL_SOURCE := kernel/motorola/sholes
 TARGET_KERNEL_CUSTOM_TOOLCHAIN := android-toolchain-eabi
  # arm-eabi-4.4.3
 
+TARGET_MODULES_SOURCE := "system/wlan/ti/wilink_6_1/platforms/os/linux/"
 
-# Connectivity - Wi-Fi
-#USES_TI_MAC80211 := true
-#ifdef USES_TI_MAC80211
-#BOARD_WPA_SUPPLICANT_DRIVER := NL80211
-#WPA_SUPPLICANT_VERSION := VER_0_8_X
-#BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_wl12xx
-#BOARD_WLAN_DEVICE := wl12xx_mac80211
-#BOARD_SOFTAP_DEVICE := wl12xx_mac80211
-#WIFI_DRIVER_MODULE_PATH := "/system/lib/modules/wl12xx_spi.ko"
-#WIFI_DRIVER_MODULE_NAME := "wl12xx_spi"
-#WIFI_FIRMWARE_LOADER := ""
-#COMMON_GLOBAL_CFLAGS += -DUSES_TI_MAC80211
-#endif
+WIFI_MODULES:
+	make -C $(TARGET_MODULES_SOURCE) HOST_PLATFORM=zoom2 KERNEL_DIR=$(KERNEL_OUT) ARCH=$(TARGET_ARCH) $(ARM_CROSS_COMPILE)
+	mv system/wlan/ti/wilink_6_1/platforms/os/linux/tiwlan_drv.ko $(KERNEL_MODULES_OUT)
 
-#TARGET_MODULES_SOURCE := "hardware/ti/wlan/mac80211/compat_wl12xx"
-#
-#WIFI_MODULES:
-#	make -C $(TARGET_MODULES_SOURCE) KERNEL_DIR=$(KERNEL_OUT) KLIB=$(KERNEL_OUT) KLIB_BUILD=$(KERNEL_OUT) ARCH=$(TARGET_ARCH) #$(ARM_CROSS_COMPILE)
-#	mv $(KERNEL_OUT)/lib/crc7.ko $(KERNEL_MODULES_OUT)
-#	mv hardware/ti/wlan/mac80211/compat_wl12xx/compat/compat.ko $(KERNEL_MODULES_OUT)
-#	mv hardware/ti/wlan/mac80211/compat_wl12xx/net/mac80211/mac80211.ko $(KERNEL_MODULES_OUT)
-#	mv hardware/ti/wlan/mac80211/compat_wl12xx/net/wireless/cfg80211.ko $(KERNEL_MODULES_OUT)
-#	mv hardware/ti/wlan/mac80211/compat_wl12xx/drivers/net/wireless/wl12xx/wl12xx.ko $(KERNEL_MODULES_OUT)
-#	mv hardware/ti/wlan/mac80211/compat_wl12xx/drivers/net/wireless/wl12xx/wl12xx_spi.ko $(KERNEL_MODULES_OUT)
-#
-#TARGET_KERNEL_MODULES := WIFI_MODULES
+TARGET_KERNEL_MODULES := WIFI_MODULES
 
 
 
